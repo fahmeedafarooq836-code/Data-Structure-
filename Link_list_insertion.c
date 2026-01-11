987be7f46da27a7aac7ce70823964159a96f972e
fahmeedafarooq836-code <fahmeedafarooq836@gmail.com>
2026-01-11 22:41:35 +0530

    Initial commit

---
+++ Link_list_insertion.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+void display(int arr[], int n)
+{
+    // traversal
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+int index_inssertion(int arr[], int n, int index, int element)
+{
+    int capacity = 100;
+    // index insertion
+    if (n >= capacity)
+    {
+        return -1;
+    }
+    for (int i = n - 1; i >= index; i--)
+    {
+        arr[i + 1] = arr[i];
+    }
+    arr[index] = element;
+    return 1;
+}
+int main()
+{
+    int arr[100] = {7, 8, 12, 27, 88};
+    int size = 5, element = 45, index = 3;
+    display(arr, size);
+    index_inssertion(arr, size, index, element);
+    size += 1;
+    display(arr, size);
+    return 0;
+}
